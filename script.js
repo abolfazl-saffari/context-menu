67a823111bc2b2d31262b5faccf97fa69241a338
Abolfazl-drx <abolfazl_drx@yahoo.com>
2021-12-07 01:37:04 -0800

    Hi

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ let $ = document
 let contextMenu = $.querySelector('.context-menu')
 $.body.addEventListener('contextmenu', (event) => {
     event.preventDefault()
-    contextMenu.style.display = 'inline'
-    contextMenu.style.left = event.pageX + 'px'
-    contextMenu.style.top = event.pageY + 'px'
+    if (contextMenu.style.display === 'none') {
+        contextMenu.style.display = 'inline'
+        contextMenu.style.left = event.pageX + 'px'
+        contextMenu.style.top = event.pageY + 'px'
+    } else {
+        contextMenu.style.left = event.pageX + 'px'
+        contextMenu.style.top = event.pageY + 'px'
+    }
 })
 $.body.addEventListener('click', () => {
     contextMenu.style.display = 'none'
